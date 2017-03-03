c04e823ff942f85e710c8f1f7d44c13ff3d7f6d8
connorotte <connorotte@gmail.com>
2017-03-03 05:25:09 +0000

    holy smokes this is getting to be difficult to remember

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,42 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id              :integer          not null, primary key
-#  name            :string
-#  email           :string
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#  password_digest :string
-#
-
 class User < ApplicationRecord
-    before_save { email.downcase! }
-    validates :name, presence: true, length: { maximum: 50}
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255},
-        format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
-    has_secure_password
-    validates :password, presence: true, length: { minimum: 5}
-    
-    def User.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+    #accessors provide a get and set function
+  attr_accessor :remember_token
+  
+  #provides database constraints
+  before_save { self.email = email.downcase }
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false } #true is implied with case sensitive
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+  # Returns the hash digest of the given string, secures a password
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
-        BCrypt::Password.create(string, cost: cost)
-    end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token used as a key for remembering sessions 
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # remembers a user
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token)) #no password authentication?
+  end
+  
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
+   # Returns true if the given token matches the digest. Verifies that the user retrieved from the database is the correct user
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
